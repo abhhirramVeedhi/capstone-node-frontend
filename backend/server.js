94980eea314c875c737d9b660b30163d217f49c7
Abhhirram Veedhi <141173291+abhhirramVeedhi@users.noreply.github.com>
2025-04-12 11:53:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -275,6 +275,10 @@ app.get("/learn", (req, res) => {
   res.render("learn");
 });
 
-app.listen(3002, () => {
-  console.log("✅ Server running on http://localhost:3002");
-});
+// app.listen(3002, () => {
+//   console.log("✅ Server running on http://localhost:3002");
+// });
+
+const serverless = require("serverless-http");
+module.exports.handler = serverless(app);
+
